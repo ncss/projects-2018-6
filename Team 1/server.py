3a7bbd48c5d43c7ed2695639ff9b997e6e4e161c
Sebastian Pauka <spauka@gmail.com>
2018-01-13 19:16:02 +1100

    Update server to use a single channel

--- Team 1/server.py
+++ Team 1/server.py
@@ -1,12 +1,14 @@
 from quokka import *
-
 from BattleStuff import *
 
 # 8 is client1 dedicated, 10 is client2 dedicated, 14 is everything else
-channels = [8, 10, 14]
+# Let's make everything run on channel 14
+channel = 14
 
+radio.config(channel=14)
 radio.enable()
 
+unaddressed_msg_queue = []
 connected_clients = []
 
 # Setup states
@@ -21,21 +23,35 @@ RESET = 6 # Reset everything
 state = POLL_CLIENTS
 
 class Client:
-    def __init__(self, channel):
-        self.channel = channel
+    def __init__(self, client_id):
+        self.id = client_id
         self.pokemon = []
         self.current_pokemon = 0
-        self.message_buffer = None
+        self.radio_buffer = []
+        self.message_buffer = ""
 
     def send_message(self, message):
         if len(message) > 250:
             raise Exception('The message is too long and we need to rewrite this code. :\'(')
-        radio.config(channel=self.channel)
-        radio.send(message)
+        radio.send("{}:{}".format(self.id, message))
+        sleep(50)
 
     def recv_message(self):
-        radio.config(channel=self.channel)
-        return radio.receive()
+        if len(self.radio_buffer) > 0:
+            return self.radio_buffer.pop(0)
+        return None
+
+    def handle_message(self, msg):
+        client_id, msg = msg.split(":", 1)
+        try:
+            client_id = int(client_id)
+        except ValueError:
+            return False
+        if client_id == self.id:
+            print("Client {} handled {}".format(self.id, msg))
+            self.radio_buffer.append(msg)
+            return True
+        return False
 
 def poll_clients():
     '''
@@ -43,36 +59,37 @@ def poll_clients():
     '''
     global channels
     global connected_clients
+    global state
 
-    # Loop the channels, listening for connecting clients
-    for channel in channels:
-        radio.config(channel=channel)
-        msg = radio.receive()
+    # Listen for connecting clients
+    if len(unaddressed_msg_queue) > 0:
+        msg = unaddressed_msg_queue.pop(0)
+        # If a client is trying to connect
         if msg == "connect":
-            # Handle the reception of a message here
-            if channel in [client.channel for client in connected_clients]:
-                client.send_message('confirm')
-                continue
-            connected_clients.append(Client(channel))
-            connected_clients[-1].send_message('confirm')
+            client_id = len(connected_clients)
+            radio.send("confirm:{}".format(client_id))
+            sleep(50) # Give the client time to receive the message
+            connected_clients.append(Client(client_id))
 
-    # If two (or more) clients have connected, start the game with the first two who connected
+    # Once two clients have connected, start the game with the first two who connected
     if len(connected_clients) >= 2:
         # start the game
         state = CLIENT_SETUP
-        connected_clients = connected_clients[:2]
-        for client in connected_clients:
-            client.send_message('server_ready')
+        radio.send("server_ready")
 
 def client_setup():
     '''
     Poll for setup data from the clients
     '''
+    global state
+    global connected_clients
     # If all clients have connected and confirmed their pokemon lists
+    # we move on to BATTLE
     if all([bool(client.pokemon) for client in connected_clients]):
+        sleep(50) # wait for confirm to be received
         state = POLL_BATTLE
-        for client in connected_clients:
-            client.send_message('start_battle')
+        radio.send("start_battle")
+        sleep(50)
         return
 
     # Loop the connected clients, listening for the pokemon choices
@@ -92,10 +109,13 @@ def poll_battle():
     '''
     Poll for incoming battle actions from clients
     '''
+    global state
     global connected_clients
 
     for c, client in enumerate(connected_clients):
         msg = client.recv_message()
+        if msg is None:
+            continue
         if msg.startswith("action:"):
             # Handle the reception of a message here
             # if the action has already been received, skip it
@@ -116,6 +136,7 @@ def run_battle():
     '''
     Run a simulation of the current turn of the pokemon battle
     '''
+    global state
     pokemon = []
 
     # process the message data
@@ -147,6 +168,7 @@ def transfer_battle():
     '''
     Transfer the buffered messages to the clients
     '''
+    global state
     global connected_clients
 
     game_over = False
@@ -176,9 +198,11 @@ def transfer_battle():
             state = POLL_BATTLE
 
 def game_finish():
+    global state
     # TODO MVP+1/2 Handle the spectating screen as required here
     state = RESET
 
+last_print = running_time()
 while True:
     state_actions = {
                      POLL_CLIENTS : poll_clients,
@@ -189,5 +213,22 @@ while True:
                      GAME_FINISH : game_finish,
                      RESET : machine.soft_reset
                     }
+
+    if last_print+1000 < running_time():
+        last_print = running_time()
+        print(state, unaddressed_msg_queue, connected_clients)
+
+    # Check if we have received a message and assign it to a client
+    msg = radio.receive()
+    if msg is not None:
+        if ":" in msg:
+            for client in connected_clients:
+                if client.handle_message(msg):
+                    break
+            else:
+                unaddressed_msg_queue.append(message)
+        else:
+            unaddressed_msg_queue.append(msg)
+
     # Run the corresponding function for the current state
     state_actions[state]()
