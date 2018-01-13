356230650f9d8e9dd872f5a5385b2314c48cad2b
Eddie Qi <eddieqi128@gmail.com>
2018-01-13 23:02:19 +1100

    aaaaa help me im dying

--- Team 2/mb_buttons.py
+++ Team 2/mb_buttons.py
@@ -1,16 +1,17 @@
 from microbit import *
 import radio
 
-SIDE_LENGTH = 2
+SIDE_LENGTH = 3
 PREFIX = "tw:btn:"
-OUT_PINS = [pin1, pin0]
-IN_PINS = [pin2, pin8]
+IN_PINS = [pin0, pin1, pin2]#, pin10]
+OUT_PINS = [pin13, pin14, pin15]#, pin16]
+TOLERANCE = 800
 
 buttons = [[False] * SIDE_LENGTH for _ in range(SIDE_LENGTH)]
 
 #display.off()
-#radio.on()
-#radio.config(channel=6)
+radio.on()
+radio.config(channel=6)
 
 def init():
 	# Set output pins to high initially
@@ -23,6 +24,9 @@ def init():
 def update_display(x, y, b):
 	display.set_pixel(x, y, 9)
 
+
+init()
+
 while True:
 	for i, out_pin in enumerate(OUT_PINS):
 		# Iterate through each output pin
@@ -30,17 +34,12 @@ while True:
 
 		row_states = []
 		for j, in_pin in enumerate(IN_PINS):
-			row_states.append(not in_pin.read_digital())
+			row_states.append(not (in_pin.read_analog() < TOLERANCE))
 
+			#print(i, j, in_pin.read_analog())
 			# Check if any buttons have changed states
-			if buttons[i][j] != row_states[j]:
-				msg = PREFIX
-				buttons[i] = row_states
-				for row in buttons:
-					msg += "".join([("1," if btn == False else ",") for btn in row])
-					msg += ";"
-				#radio.send(msg)
-				print(msg)
+			#if buttons[i][j] != row_states[j]:
+			#	
 
 		buttons[i] = row_states
 
@@ -51,3 +50,14 @@ while True:
 	for i in range(len(buttons)):
 		for j in range(len(buttons[i])):
 			display.set_pixel(i, j, (9 if buttons[i][j] else 0))
+	#sleep(100)
+
+	# Send button states
+	msg = PREFIX
+	buttons[i] = row_states
+	for row in buttons:
+		msg += "".join([("1," if btn == False else ",") for btn in row])
+		msg += ";"
+
+	radio.send(msg)
+	print(msg)
