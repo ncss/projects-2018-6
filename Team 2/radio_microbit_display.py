776a776158149028d69f4d45d6a9927cbf53c625
xtilda <xtilda@users.noreply.github.com>
2018-01-14 07:03:22 +1100

    Proper Display

--- Team 2/radio_microbit_display.py
+++ Team 2/radio_microbit_display.py
@@ -1,31 +1,24 @@
 from microbit import *
 import radio
-import music
 
 radio.on()
 radio.config(channel=6)
 
-foot_sound = 'C6:6'
-hand_sound = 'C4:6'
-
-timer = 10
-
+display.scroll('Ready?')
 while True:
     msg = radio.receive()
     
-    if msg:
-        cmd, position = msg.split(':')
-        if cmd == 'dsp_side':
-            if position == 'RIGHT':
-                display.show(Image.ARROW_E, wait = False)
-            elif position == 'LEFT':
-                display.show(Image.ARROW_W, wait = False)
-        
-        if cmd == 'dsp_limb':
-            if position == 'HAND':
-                music.play(hand_sound, wait = False)
-            elif position == 'FOOT':
-                music.play(foot_sound, wait = False)
+    if msg and msg.s  tartswith('tw:dsp:'):
+        print(msg)
+        protocol, cmd, position = msg.split(':')
+        if position == 'LHAND':
+            display.show(Image.ARROW_NW)
+        elif position == 'RHAND':
+            display.show(Image.ARROW_NE)
+        elif position == 'RFOOT':
+            display.show(Image.ARROW_SE)
+        elif position == 'LFOOT':
+            display.show(Image.ARROW_SW)
             
-        sleep(timer*1000)
-        timer = timer*0.9
+        #sleep(timer*1000)
+        #timer = timer*0.9
