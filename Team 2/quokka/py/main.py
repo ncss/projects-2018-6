16e1443c84d6aba1aaa966181610874ebefac982
xtilda <xtilda@users.noreply.github.com>
2018-01-14 03:43:20 +1100

    Final

--- Team 2/quokka/py/main.py
+++ Team 2/quokka/py/main.py
@@ -8,6 +8,7 @@ b = 0
 colours =  ['RED', 'GREEN', 'YELLOW', 'BLUE']
 sides = ['L', 'R']
 limbs = ['HAND', 'FOOT']
+colours_dict = {"GREEN":0, "YELLOW":1, "BLUE":2, "RED":3}
 
 start_statements = ['Ready?', '3' , '2', '1']
 
@@ -37,7 +38,6 @@ def body_show(current_side, current_limb):
 
 #reset the turn - screen, timer, colours, neopixels
 def turn_reset():
-    #quokka.sleep(int(timer*1000))
     r = 0
     g = 0
     b = 0
@@ -68,21 +68,21 @@ while True:
 
     timer = 10
     lives = 2
-    points = -1
+    points = 0
 
     # start screen
-
+    started = False
     if quokka.buttons.a.was_pressed():
+
         start_sequence()
 
         #Twister
 
         while lives > 0:
-
+            prev_sum = [0]*4
             #control turns by button pressed
-            if quokka.buttons.b.was_pressed():
-
-                points += 1
+            if quokka.buttons.b.was_pressed() or started:
+                started = True
                 quokka.radio.send('tw:scr:' + str(points))
             #Gameplay positions
 
@@ -95,15 +95,47 @@ while True:
                 current_side = random.choice(sides)
                 body_show(current_side, current_limb)
 
-                print(current_colour + ' ' + current_side + current_limb)
+                start_time = quokka.running_time()
+                elapse_time = quokka.running_time() - start_time
+
+
+                while elapse_time < 5000:
+                    msg = quokka.radio.receive()
+                    if current_colour == "RED":
+                        quokka.sleep(2000)
+                        points += 1
+                        break
+
+                    if msg:
+                        try:
+                            protocol, cmd, array = msg.split(":")
+                            if protocol == "tw" and cmd=="btn":
+                                buttons = []
+                                for line in array.split(';'):
+                                    buttons.append(line.split(","))
+                        except ValueError:
+                            continue
+
+                        # list of integers, showing num of buttons pressed in each row
+                        #if current_colour
+                        curr_sum = sum(map(bool, buttons[colours_dict[current_colour]]))
+
+
+                        if prev_sum[colours_dict[current_colour]] != curr_sum:
+                        #colour_pressed = any(buttons[colours_dict[current_colour]])
+                        #if colour_pressed:
+                            prev_sum[colours_dict[current_colour]] = curr_sum
+                            points += 1
+                            break
+
+                    elapse_time = quokka.running_time() - start_time
+
                 turn_reset()
 
-            if quokka.buttons.c.was_pressed():
-                lives -= 1
 
-            #Gameover sequence
-        if lives <= 0:
-            quokka.radio.send('tw:scr:' + 'Gameover :(. You earned ' + str(points) + '!')
+
+            if quokka.buttons.c.was_pressed():
+                quokka.radio.send('tw:scr:-1')
 
                 #timer = turn_reset(timer)
 
@@ -116,20 +148,7 @@ while True:
                 turn_over = False
                 while not turn_over:
                     msg = quokka.radio.receive()
-                    if msg and msg.startswith("tw:btn:"):
-                        buttons = []
-                        msg = msg.split(":")[-1]
-                        for line in msg.split():
-                            buttons.append(msg.split(","))
 
-                        # list of integers, showing num of buttons pressed in each row
-                        num_in_each_row = []
-                        for row in buttons:
-                            count = 0
-                            for but in row:
-                                if but:
-                                    count += 1
-                            num_in_each_row.append(count)
 
                         delta = []
 
