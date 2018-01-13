10fc2b371a5608305e7ced7a04d076c1e66dd7e0
melvines <mel20007005595401@gmail.com>
2018-01-14 03:49:36 +1100

    microbit score

--- Team 2/twister_score.py
+++ Team 2/twister_score.py
@@ -1,15 +1,31 @@
 from microbit import *
 
 import radio
-
+current_score = 0
 radio.on()
-radio.config(channel = 6)
-prefix = 'tw:scr'
+radio.config(channel=6)
+prefix = 'tw:scr:9'
 
 while True:
     
-    score = radio.receive()
+    msg = radio.receive()
     
-    if score:
-        if score.startswith(prefix):
-                display.show(score)
+    if msg:
+        print(msg)
+        try:
+            game, scr, score = msg.split(':')
+        except ValueError:
+            continue
+        if game == 'tw' and scr == 'scr':
+            current_score = int(score)
+            if current_score == -1:
+                display.scroll('Game Over')
+                display.show(Image.NO)
+            else:
+                display.scroll(str(current_score))
+    elif current_score == 0:
+        display.show('0')
+    else:  
+        display.scroll(str(current_score))
+
+        
